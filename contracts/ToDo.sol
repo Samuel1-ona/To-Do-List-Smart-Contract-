97b35e1d31e2e2ce3a10a9174594642fc1ac4cd7
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-13 10:15:41 +0100

    intial commit

---
+++ contracts/ToDo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract ToDo {
+    uint256 public taskId;
+
+    struct Lists {
+        uint256 id;
+        string title;
+        string descriptions;
+        bool isDone;
+    }
+
+    
+}
